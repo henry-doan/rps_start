1c9280507e1ac85479ab5f848371855dffb95cb1
Henry Doan <henrydoan96@gmail.com>
2020-09-17 18:22:11 -0600

    sudo code start

---
+++ app.js
@@ -0,0 +1,16 @@
+// test to make sure js is connect 
+// alert 
+// grab the elements on html side
+// grab display
+// r p s 
+// document.getElementById('idName')
+
+// display msg , .innerHTML
+
+// add event listeners , r p s 
+// click event then run a function
+
+// function of what happens when you click on rock
+// logic, computer chooses r p s
+// logic, comparison , win logic
+// display the msg
